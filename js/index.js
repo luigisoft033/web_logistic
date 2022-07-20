5cc064d34a1608662a7934f1c971bd1515e6f954
Luigisoft033 <luigisoft033@gmail.com>
2022-07-20 11:28:10 -0500

    animacion

---
+++ js/index.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal();
+    
+    sr.reveal('.header', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px',
+    });
+    sr.reveal('.menu-navegacion', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+    });
+    sr.reveal('.slider--index', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px',
+    });
+    sr.reveal('.index--seMas', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+    });
+    sr.reveal('.if-you-are', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '300px',
+    });
+    sr.reveal('.index--categorias-clientes', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-300px',
+    });
+    sr.reveal('.franja', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-300px',
+    });
+    sr.reveal('.index--categories-clientes__image-container', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '200px',
+    });
+    sr.reveal('.index--franja-clientes', {
+        duration: 2000,
+        origin: 'right',
+        distance: '200px',
+    });
+    sr.reveal('.index--servicios__container', {
+        duration: 3000,
+        origin: 'left',
+        distance: '200px',
+    });
+    
+    
+    
+
+    
